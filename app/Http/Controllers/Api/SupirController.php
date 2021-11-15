d938c8f02b6d55cf6c57e04cdd5739320fb7b653
shellrean <reanyusufali@gmail.com>
2021-11-15 22:12:07 +0700

    add: crud supir

---
+++ app/Http/Controllers/Api/SupirController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Supir;
+use Illuminate\Http\Request;
+
+class SupirController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $supirs = Supir::paginate();
+        return response()->json($supirs);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'no_reg' => 'required',
+            'nama_lengkap' => 'required',
+            'alamat' => 'required',
+            'jk' => 'required|in:P,L'
+        ]);
+
+        $supir = Supir::create($request->all());
+        return response()->json($supir);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Supir $supir)
+    {
+        return response()->json($supir);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Supir $supir)
+    {
+        $request->validate([
+            'no_reg' => 'required',
+            'nama_lengkap' => 'required',
+            'alamat' => 'required',
+            'jk' => 'required|in:L,P'
+        ]);
+
+        $supir->no_reg = $request->no_reg;
+        $supir->nama_lengkap = $request->nama_lengkap;
+        $supir->alamat = $request->alamat;
+        $supir->jk = $request->jk;
+        $supir->save();
+
+        return response()->json($supir);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Supir $supir)
+    {
+        $supir->delete();
+        return response()->json(['message' => 'supir berhasil dihapus']);
+    }
+}
---
+++ database/migrations/2021_11_15_144821_create_supirs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupirsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supirs', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_reg');
+            $table->string('nama_lengkap');
+            $table->string('alamat');
+            $table->string('jk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supirs');
+    }
+}
